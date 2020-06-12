6d94f027696cf2d55da35651c77c7259d219106e
今村子竜 <imamura-shiryu@arrows-systems.co.jp>
2020-06-12 15:45:32 +0900

    2020/6/12/15:45...sample07変更

--- php7basic/php/sample07.php
+++ php7basic/php/sample07.php
@@ -22,11 +22,11 @@
 <?php
 
 //print(date('n/j(D)',time() + 60*60*24));
-for($i=1;$i<=365;$i++){
+for($i=1;$i<=365;$i++):
     $date = strtotime('+' . $i . 'day');
     print(date('n/j(D)',$date));
     print("\n");
-}
+endfor;
 ?>
 
 </pre>
