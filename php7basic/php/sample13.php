5cd3796f935fe977f987d772db9c4fd75394026a
今村子竜 <imamura-s@arrows-systems.co.jp>
2020-06-16 09:46:09 +0900

    2020/6/16

---
+++ php7basic/php/sample13.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+$success = file_put_contents('../../../../news_data/news.txt','2018-06-01 ホームページリニューアル');
+if($success){
+    print('ファイル書き込み完了');
+}
+else{
+    print('書き込み失敗,フォルダの権限など確認してください');
+}
+?>
+</pre>
+</main>
+</body>    
+</html>
