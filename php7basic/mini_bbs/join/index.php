98c632b6b344b8550004ce6609d6fd74b40cf3cd
今村子竜 <imamura-s@arrows-systems.co.jp>
2020-06-22 09:55:49 +0900

    2020/6/22/9:55

--- php7basic/mini_bbs/join/index.php
+++ php7basic/mini_bbs/join/index.php
@@ -22,8 +22,7 @@ if (!empty($_POST)){
         }
     }
     if (empty($error)){
-        $member = $db->prepare('SELECT COUNT(*) AS cnt
-        FROM members WHERE email=?');
+        $member = $db->prepare('SELECT COUNT(*) AS cnt FROM members WHERE email=?');
         $member->execute(array($_POST['email']));
         $record = $member->fetch();
         if ($record['cnt'] > 0) {
